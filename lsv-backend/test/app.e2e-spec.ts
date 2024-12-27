903fb7b506ca3f8aac2fce1bf8bcef09f87063cf
José D Quijada <quijadajosed@gmail.com>
2024-12-27 01:08:21 -0300

    feat: add test service to Docker Compose and implement e2e test for user registration

--- lsv-backend/test/app.e2e-spec.ts
+++ lsv-backend/test/app.e2e-spec.ts
@@ -21,4 +21,24 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+  it('/auth/register (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        email: "johndoe@example.com",
+        password: "hashedPassword",
+        firstName: "John",
+        lastName: "Doe",
+        age: 30,
+        isRightHanded: true,
+        role: "user"
+      })
+      .expect(201)
+      .expect(response => {
+        console.log('response', response);
+
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.email).toBe('example@example.com');
+      });
+  });
 });
