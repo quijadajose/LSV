138399945e6dd1dfa6dafd4a60dc59557c8ee4fc
José D Quijada <quijadajosed@gmail.com>
2024-12-31 19:35:56 -0300

    feat: add login functionality with JWT authentication, update user DTOs and fix update user

--- lsv-backend/src/auth/domain/ports/token.service/token.service.interface.ts
+++ lsv-backend/src/auth/domain/ports/token.service/token.service.interface.ts
@@ -1,6 +1,7 @@
+import { JwtPayload } from "src/auth/interfaces/jwt-payload.interface";
 import { User } from "src/shared/domain/entities/user";
 
 export interface TokenService {
     generateToken(user: User, expiresIn?: string): string;
-    verifyToken(token: string): User;
+    verifyToken(token: string): JwtPayload;
 }
