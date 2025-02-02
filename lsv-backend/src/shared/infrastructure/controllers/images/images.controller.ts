933b97339961b05bc132b74d4f0c0eb7a0011d25
José Quijada <quijadajosed@gmail.com>
2025-02-02 20:26:21 -0300

    feat: remove AppController and AppService, add ImagesController for image handling

---
+++ lsv-backend/src/shared/infrastructure/controllers/images/images.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, NotFoundException, Param, Res, StreamableFile } from '@nestjs/common';
+import { createReadStream, existsSync } from 'fs';
+import { join } from 'path';
+import { Public } from 'src/auth/infrastructure/decorators/public.decorator';
+import { lookup } from 'mime-types';
+import type { Response } from 'express';
+
+@Controller('images')
+export class ImagesController {
+    @Public()
+    @Get(':folder/:filename')
+    getImage(
+        @Param('folder') folder: string,
+        @Param('filename') filename: string,
+        @Res({ passthrough: true }) res: Response,
+    ): StreamableFile {
+        const filePath = join(process.cwd(), 'uploads', folder, filename);
+
+        if (!existsSync(filePath)) {
+            throw new NotFoundException('Image not found');
+        }
+
+        const mimeType = lookup(filePath) || 'application/octet-stream'; // Detecta el tipo de contenido
+
+        const fileStream = createReadStream(filePath);
+        res.set({
+            'Content-Type': mimeType,
+            'Content-Disposition': `inline; filename="${filename}"`,
+        });
+
+        return new StreamableFile(fileStream);
+    }
+}
