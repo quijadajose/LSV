137e61e0ab2c61b63ae9fb6670836739225e6154
Jose Quijada <quijadajosed@gmail.com>
2026-03-09 14:12:21 -0300

    feat: add Sentry integration for error tracking in frontend

--- lsv-frontend/src/main.tsx
+++ lsv-frontend/src/main.tsx
@@ -4,6 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+const SENTRY_DSN = import.meta.env.SENTRY_DSN;
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  // Enable logs to be sent to Sentry
+  enableLogs: true
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
