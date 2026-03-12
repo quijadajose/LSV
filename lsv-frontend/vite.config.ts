628f7c666073ed75c585eb24236e2b8e3118e053
Jose Quijada <quijadajosed@gmail.com>
2026-03-12 16:52:23 -0300

    ci: Implement GitHub Pages deployment for the frontend using GitHub Actions and update Vite configuration.

--- lsv-frontend/vite.config.ts
+++ lsv-frontend/vite.config.ts
@@ -5,6 +5,7 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/LSV/',
   server: {
     port: 8080,
   },
